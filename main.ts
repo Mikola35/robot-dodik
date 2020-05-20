83b7796b60c1b9cf74bbf3c519fa1842291faa32
Nikolay M <mikola_foto@mail.ru>
2020-05-20 11:02:31 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,42 @@
 input.onButtonPressed(Button.A, function () {
-    левое_плечо(вниз, 1)
+    левое_плечо(вверх, 100)
+    левое_плечо(вверх, 50)
+    левое_плечо(вверх, 0)
 })
 function левое_плечо (string: string, num: number) {
     if (string == вверх) {
-        robotbit.Servo(robotbit.Servos.S1, 180)
+        Угол_1 = pins.map(
+        num,
+        0,
+        100,
+        90,
+        180
+        )
+        robotbit.Servo(robotbit.Servos.S1, Угол_1)
     } else if (string == вниз) {
-        robotbit.Servo(robotbit.Servos.S1, 0)
+        Угол_1 = pins.map(
+        num,
+        0,
+        100,
+        0,
+        90
+        )
+        robotbit.Servo(robotbit.Servos.S1, Угол_1)
     } else {
         robotbit.Servo(robotbit.Servos.S1, 90)
     }
+    basic.pause(500)
 }
 function подставка (string: string, num: number) {
 	
 }
-input.onButtonPressed(Button.AB, function () {
-    левое_плечо(середина, 1)
-})
-input.onButtonPressed(Button.B, function () {
-    левое_плечо(вверх, 1)
-})
 function правое_плечо (string: string, num: number) {
 	
 }
+let Угол_1 = 0
 let вниз = ""
 let вверх = ""
-let середина = ""
-середина = "середина"
+let середина = "середина"
 вверх = "вверх"
 вниз = "вниз"
 let влево = "влево"
