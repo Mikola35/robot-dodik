9eee80b4e7389c9f213e94cdfe66eed7c2f2fc36
Nikolay M <mikola_foto@mail.ru>
2020-05-21 10:04:00 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,57 @@
 input.onButtonPressed(Button.A, function () {
-    левое_плечо(вверх, 100)
-    левое_плечо(вверх, 50)
-    левое_плечо(вверх, 0)
+    for (let index = 0; index < 4; index++) {
+        левая_рука(вверх, 100)
+        basic.pause(500)
+        левая_рука(вниз, 100)
+        basic.pause(500)
+    }
 })
-function левое_плечо (string: string, num: number) {
+input.onButtonPressed(Button.AB, function () {
+    правая_рука(середина, 100)
+    левая_рука(середина, 100)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        правая_рука(вверх, 100)
+        basic.pause(500)
+        правая_рука(вниз, 100)
+        basic.pause(500)
+    }
+})
+function левая_рука (string: string, num: number) {
     if (string == вверх) {
+        Угол_2 = pins.map(
+        num,
+        0,
+        100,
+        90,
+        180
+        )
+        robotbit.Servo(robotbit.Servos.S1, Угол_2)
+    } else if (string == вниз) {
+        Угол_2 = pins.map(
+        num,
+        0,
+        100,
+        90,
+        0
+        )
+        robotbit.Servo(robotbit.Servos.S1, Угол_2)
+    } else {
+        robotbit.Servo(robotbit.Servos.S1, 90)
+    }
+}
+function правая_рука (string: string, num: number) {
+    if (string == вверх) {
+        Угол_1 = pins.map(
+        num,
+        0,
+        100,
+        90,
+        0
+        )
+        robotbit.Servo(robotbit.Servos.S2, Угол_1)
+    } else if (string == вниз) {
         Угол_1 = pins.map(
         num,
         0,
@@ -12,32 +59,25 @@ function левое_плечо (string: string, num: number) {
         90,
         180
         )
-        robotbit.Servo(robotbit.Servos.S1, Угол_1)
-    } else if (string == вниз) {
-        Угол_1 = pins.map(
-        num,
-        0,
-        100,
-        0,
-        90
-        )
-        robotbit.Servo(robotbit.Servos.S1, Угол_1)
+        robotbit.Servo(robotbit.Servos.S2, Угол_1)
     } else {
-        robotbit.Servo(robotbit.Servos.S1, 90)
+        robotbit.Servo(robotbit.Servos.S2, 90)
     }
-    basic.pause(500)
-}
-function подставка (string: string, num: number) {
-	
-}
-function правое_плечо (string: string, num: number) {
-	
 }
 let Угол_1 = 0
+let Угол_2 = 0
 let вниз = ""
 let вверх = ""
-let середина = "середина"
+let середина = ""
+середина = "середина"
 вверх = "вверх"
 вниз = "вниз"
 let влево = "влево"
 let вправо = "вправо"
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
